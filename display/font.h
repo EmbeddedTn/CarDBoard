315aa4190ecdd47e691dd30f454239788449a53c
Degra02 <filippodegrandi02@gmail.com>
2024-01-25 11:02:16 +0100

    Init project

---
+++ display/font.h
@@ -0,0 +1,13 @@
+/*
+ * font.h
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: degra
+ */
+#include <stdint.h>
+#ifndef FONT_H_
+#define FONT_H_
+
+extern const uint8_t font[];
+
+#endif /* FONT_H_ */
---
+++ display/slb.h
@@ -0,0 +1,8 @@
+#include <ti/grlib/grlib.h>
+#ifndef SLB_H_
+#define SLB_H_
+
+
+extern const Graphics_Image slb_image;
+
+#endif /* SLB_H_ */
