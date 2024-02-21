36852d6ee1d5fcd6b5d4a1478870eab979146dc6
topongo <lorenzo.bodini.private@gmail.com>
2024-02-21 17:33:14 +0100

    remove hotspot password

--- osm/osm.ino
+++ osm/osm.ino
@@ -5,10 +5,6 @@
 #include <HardwareSerial.h>
 #include "secrets.h"
 
-//Your Domain name with URL path or IP address with path
-// const char* serverName = "http://192.168.1.106:1880/get-sensor";
-
-// see : "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-34.44076&lon=-58.70521";
 #define NOMINATIM_URL "https://nominatim.openstreetmap.org/reverse?format=jsonv2"
 #define OVERPASS_URL "http://overpass-api.de/api/interpreter"
 #define OVERPASS_QUERY1 "[out:json][maxsize:20000]; way[highway][highway!~\"(footway|track|cycleway|steps|pedestrian|construction)\"][access!~\"(no|private)\"]("
@@ -27,7 +23,6 @@ HardwareSerial GPSSerial(1);
 #define PCSerial Serial
 #define RATE 9600
 
-// Define TinyGPS++ object
 TinyGPSPlus gps;
 HTTPClient** http_nom;
 HTTPClient** http_op;
